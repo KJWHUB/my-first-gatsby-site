6a56ed63b8b7f12e20f006f8d5ebdb2c48fa9d7a
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-02-20 18:20:38 +0900

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -7,20 +7,27 @@ const BlogPage = ({ data }: PageProps<Queries.BlogTitleQuery>) => {
   console.log(data);
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter?.title}</h2>
+          <p>Posted: {node.frontmatter?.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query BlogTitle {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
