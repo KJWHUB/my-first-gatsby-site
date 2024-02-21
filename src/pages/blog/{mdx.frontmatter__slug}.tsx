fc89e0fde05b75a60891406044f3b8903f7801a1
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-02-21 14:56:30 +0900

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { graphql, type HeadProps, type PageProps } from "gatsby";
+import { graphql, Link, type HeadProps, type PageProps } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
@@ -7,9 +8,15 @@ const BlogPost = ({ data, children }: PageProps<Queries.BlogPostQuery>) => {
   const pageTitle = data.mdx?.frontmatter?.title || "";
   const date = data.mdx?.frontmatter?.date || "";
 
+  const image = getImage(data.mdx?.frontmatter?.hero_image?.childImageSharp?.gatsbyImageData ?? null);
+
   return (
     <Layout pageTitle={pageTitle}>
       <p>{date}</p>
+      {image && <GatsbyImage image={image} alt={data.mdx?.frontmatter?.hero_image_alt || "default alt"} />}
+      <p>
+        Photo Credit: <Link to={data?.mdx?.frontmatter?.hero_image_credit_link || ""}>{data?.mdx?.frontmatter?.hero_image_credit_text || ""}</Link>
+      </p>
       {children}
     </Layout>
   );
@@ -20,7 +27,15 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
